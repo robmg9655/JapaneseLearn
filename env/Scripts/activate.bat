ddfdf339192073b010d632340d4640471a257fed
robmg9655 <gostmon96@gmail.com>
2025-02-04 10:20:20 +0100

    virtual enviroment

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\PythonCourse\env
+set VIRTUAL_ENV=E:\JapaneseLearn\env
 
 if not defined PROMPT set PROMPT=$P$G
 
